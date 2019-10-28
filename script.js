e39f099689c92eace0b6fb99957c7e4a5d816437
hewww <50962854+hewww@users.noreply.github.com>
2019-10-28 20:13:38 +0100

    Initial Commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://radiant-peak-82947.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'https://radiant-peak-82947.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'http://trello.com/1/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
